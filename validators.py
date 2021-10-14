8af4ad09d2bfbec3697fc8728554e39a74d98528
Nick-Riddle <kolya.bezrukiy.03@gmail.com>
2021-10-14 21:33:12 +0300

    new-changes

--- validators.py
+++ validators.py
@@ -1,18 +1,49 @@
+class NotString(Exception):
+    def __init__(self, text):
+        self.text = text
+
+
+class TooManyOrLessLetters(Exception):
+    def __init__(self, text):
+        self.text = text
+
+
+class TooManyOrLessYears(Exception):
+    def __init__(self, text):
+        self.text = text
+
+
+class WrongOption(Exception):
+    def __init__(self, text):
+        self.text = text
+
+
 def check_name(name):
-    if not name.isalpha():
-        return 'Вы ввели не строку...'
-    elif not 2 < len(name) < 20:
-        return 'Строка должна содержать в себе не более 20 и не менее 2 символов...'
+    try:
+        if not name.isalpha():
+            raise NotString('Вы ввели не строку...')
+        elif not 2 < len(name) < 20:
+            raise TooManyOrLessLetters('Строка должна содержать в себе не более 20 и не менее 2 символов...')
+    except NotString as ns:
+        return ns
+    except TooManyOrLessLetters as tmlt:
+        return tmlt
 
 
 def check_age(age):
-    print('dfjsgkjdfgksdfgk')
-    if not age.isdigit():
+    try:
+        age = int(age)
+        if not 2 < age < 102:
+            raise TooManyOrLessYears('Вам должно быть от 2-102...')
+    except ValueError:
         return 'Вы ввели не число...'
-    elif not 2 < age < 102:
-        return 'Вам должно быть от 2-102...'
+    except TooManyOrLessYears as tmly:
+        return tmly
 
 
 def check_sex(answer):
-    if answer not in ('Мужской', 'Женский', 'Назад'):
-        return 'Вы не можете ввести другое значение...'
+    try:
+        if answer not in ('Мужской', 'Женский', 'Назад'):
+            raise WrongOption('Вы не можете ввести другое значение...')
+    except WrongOption as wo:
+        return wo
